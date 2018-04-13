c00dc46e09a76acc4a30590c919405b39c320e07
alkc <alexander.koc@slu.se>
2018-04-13 19:13:20 +0200

    Expand genetics module

--- genetics.py
+++ genetics.py
@@ -3,10 +3,63 @@ from util import Color
 import random
 
 Chromatid = namedtuple("Chromatid", ["coh", "sep", "ali", "R", "G", "B"])
+Phenotype = namedtuple("Phenotype", ["behavior", "color"])
+Egg = namedtuple("Egg", ["genome", "phenotype"])
 
 
-def mutate_weight():
-    pass
+def is_chromatid(namedtuple):
+    return type(namedtuple).__name__ == "Chromatid"
+
+
+def get_crossover_gametes(gametes):
+    return gametes
+
+
+def get_mutated_gametes(gametes):
+    return gametes
+
+
+def get_phenotype(boid_genome):
+    chromatid_a, chromatid_b = boid_genome
+
+    behavior = get_boid_weights(chromatid_a, chromatid_b)
+    color = get_boid_color(chromatid_a, chromatid_b)
+
+    return Phenotype(behavior, color)
+
+
+def get_baby_boid(mate_1, mate_2):
+
+    gametes = [random.choice(mate_1), random.choice(mate_2)]
+    gametes = get_mutated_gametes(gametes)
+    gametes = get_crossover_gametes(gametes)
+    boid_genome = gametes
+    phenotype = get_phenotype(boid_genome)
+
+    return Egg(boid_genome, phenotype)
+
+
+def get_next_generation(individual_genomes, nbr_boids_in_next_gen):
+
+    individuals = list(range(len(individual_genomes)))
+    next_generation = list()
+
+    for i in range(nbr_boids_in_next_gen):
+
+        mate_1 = random.choice(individuals)
+        mate_2 = random.choice(individuals)
+
+        while mate_1 is mate_2:
+            mate_2 = random.choice(individuals)
+
+        new_boid = get_baby_boid(mate_1, mate_2)
+        next_generation.append(new_boid)
+
+    flock = dict()
+    flock['colors'] = [x.phenotype.color for x in next_generation]
+    flock['weights'] = [x.phenotype.behavior for x in next_generation]
+
+    return flock
 
 
 def generate_random_chromatid(random_weights, random_colors, default_colors=(0, 0, 0)):
@@ -27,8 +80,33 @@ def get_color_genes(boid_chromatid):
     return (boid_chromatid.R, boid_chromatid.G, boid_chromatid.B)
 
 
-def get_weights(boid_chromatid):
-    pass
+def get_weight_genes(chromatid):
+
+    genes = dict()
+    # TODO: Break out to func parameter
+    weight_genes = ['coh', 'ali', 'sep']
+
+    for gene in weight_genes:
+        genes[gene] = chromatid._asdict()[gene]
+
+    return genes
+
+
+def get_boid_weights(chromatid_a, chromatid_b):
+
+    weights = dict()
+
+    genes_a = get_weight_genes(chromatid_a)
+    genes_b = get_weight_genes(chromatid_b)
+
+    def get_weight_mean(a, b):
+        return (a+b)/2
+
+    for gene in genes_a.keys():
+        a, b = (genes_a[gene], genes_b[gene])
+        weights[gene] = get_weight_mean(a, b)
+
+    return weights
 
 
 def get_boid_color(chromatid_a, chromatid_b):
