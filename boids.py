2c7c9e947a91343fab0c1ed05a636d964a9f2ec0
alkc <alexander.koc@slu.se>
2018-04-14 15:30:27 +0200

    Implement breeding, selection, generations and genetics in main simulation script

--- boids.py
+++ boids.py
@@ -5,12 +5,14 @@ import pygame
 import sys
 
 from util import *
+from genetics import *
 from rules import *
 from graphics import *
 
 # Simulation settings:
 habitat_size = (500, 500)
 nbr_boids = 100
+min_nbr_boids = 50
 
 # Flocking rules weights:
 cohesion_weight = 0.1
@@ -43,8 +45,55 @@ velocities = set_speed(velocities, boid_min_speed, boid_min_speed + 1)
 
 simulation = True
 
+boid_colors, boid_genomes, weights = get_random_flock(nbr_boids)
+
+
+def get_weight_from_flock(flock, weight):
+
+    return np.array([b[weight] for b in flock['weights']])
+
+
+def get_colors_from_flock(flock):
+    return [b["color"] for b in flock]
+
+
+cohesion_weight = weights['coh']
+separation_weight = weights['sep']
+align_weight = weights['ali']
+
+mutable_lists = [boid_colors, boid_genomes,
+                 cohesion_weight, align_weight, separation_weight]
+
+curr_nbr_boids = nbr_boids
+
+# print(separation_weight)
+# print(np.shape(separation_weight))
+
+# exit()
+
+
+def filter_list_by_boid_killist(input_list, kill_list):
+    return [input_list[i] for i in range(len(input_list)) if i not in kill_list]
+
 
 while simulation:
+
+    if curr_nbr_boids < min_nbr_boids:
+
+        # TODO: Rewrite flock ffs
+        # print(type(genomes))
+        boid_colors, boid_genomes, weights = get_next_generation(
+            boid_genomes, nbr_boids)
+        # print(type(flock))
+        cohesion_weight = weights['coh']
+        separation_weight = weights['sep']
+        align_weight = weights['ali']
+        # print(np.shape(separation_weight))
+        positions = [get_random_position(habitat_size)
+                     for x in range(nbr_boids)]
+        velocities = [get_random_velocity() for x in range(nbr_boids)]
+        velocities = set_speed(velocities, boid_min_speed, boid_min_speed + 1)
+        curr_nbr_boids = nbr_boids
 
     if len(positions) < 1:
         print("All boids are dead")
@@ -74,16 +123,28 @@ while simulation:
 
     if len(collisions) > 0:
         collisions = list(collisions)
-        positions = remove_collided_boids(positions, collisions)
-        velocities = remove_collided_boids(velocities, collisions)
+        positions = remove_collided_boids(positions, collisions, axis=0)
+        velocities = remove_collided_boids(velocities, collisions, axis=0)
+        cohesion_weight = remove_collided_boids(
+            cohesion_weight, collisions, axis=0)
+        align_weight = remove_collided_boids(align_weight, collisions, axis=0)
+        separation_weight = remove_collided_boids(
+            separation_weight, collisions, 0)
+
         # TODO: Fix this :
         boid_colors = [boid_colors[i]
                        for i in range(len(boid_colors)) if i not in collisions]
+        boid_genomes = [boid_genomes[i]
+                        for i in range(len(boid_genomes)) if i not in collisions]
+        curr_nbr_boids -= len(collisions)
         continue
 
     old_velocities = velocities.copy()
 
     # Apply rules
+    # print(separation_weight)
+    # print(separation_vector)
+
     separation_vector = separation_rule(positions, neighbor_lists,
                                         displacement_vectors, distances,
                                         min_distance_to_other_boids)
@@ -93,9 +154,11 @@ while simulation:
     cohesion_vector = cohesion_rule(positions, neighbor_lists)
 
     # Apply weights
-    separation_vector *= separation_weight
-    alignment_vector *= align_weight
-    cohesion_vector *= cohesion_weight
+    # print(separation_vector)
+
+    separation_vector *= separation_weight[:, None]
+    alignment_vector *= align_weight[:, None]
+    cohesion_vector *= cohesion_weight[:, None]
 
     # Accelerate accordingly:
     velocities = old_velocities.copy()
