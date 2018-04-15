9b25d5f991046fbd78b7f7a94cba683f4d29915e
alkc <alexander.koc@slu.se>
2018-04-15 16:22:36 +0200

    Make boids bigger

--- graphics.py
+++ graphics.py
@@ -4,4 +4,4 @@ import pygame
 def draw_boids(boid_positions, screen, boid_colors):
     for boid_position, boid_color in zip(boid_positions, boid_colors):
         x, y = boid_position.astype("int")
-        pygame.draw.circle(screen, boid_color, (x, y), 2, 0)
+        pygame.draw.circle(screen, boid_color, (x, y), 4, 0)
